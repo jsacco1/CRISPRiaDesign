a308289704c8ad1e3e936bc79bc33db6e5846652
jsacco1 <jsacco001@gmail.com>
2024-08-28 16:54:15 -0700

    vienna rna installed via pip

--- bin/installVienna.sh
+++ bin/installVienna.sh
@@ -1,14 +1,20 @@
 # install Vienna
 # Accessed 8/27/24
 
+# PIP INSTALL
+conda activate library
+pip install vienna
 
-wget https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_2_x/ViennaRNA-2.2.5.tar.gz 
-tar -zxvf ViennaRNA-2.2.5.tar.gz
-cd ViennaRNA-2.2.5
+# IF INSTALLING FROM SOURCE
+# wget https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_6_x/ViennaRNA-2.6.4.tar.gz
 
-./configure --prefix="/Users/jamessacco/dev/CRISPRiaDesign/ViennaRNA-2.2.5"
-./configure --disable-openmp
-make
-make install
-#export PATH="/Users/jamessacco/dev/CRISPRiaDesign/ViennaRNA-2.2.5/bin:$PATH"
-#RNAfold --version
+
+# #wget https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_2_x/ViennaRNA-2.2.5.tar.gz 
+# tar -zxvf ViennaRNA-2.6.4.tar.gz
+# cd ViennaRNA-2.6.4
+
+# ./configure --prefix="/Users/jamessacco/dev/CRISPRiaDesign/ViennaRNA-2.6.4"
+# ./configure --disable-openmp
+# make
+# sudo make install
+# RNAfold --version
